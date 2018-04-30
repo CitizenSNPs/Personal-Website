0cd6fddcc702c7d5bd0be3fc47b899b791d3f209
DPK <35163005+CitizenSNPs@users.noreply.github.com>
2018-04-30 00:51:18 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$('.navbar a').mouseover(function(){
+  $(this).css({'color':'#66fcf1'});
+});
+
+$('.navbar a').mouseout(function(){
+  $(this).css({'color':'white'})
+});
+
+$('.social').mouseover(function(){
+  $(this).css({'color':'#66fcf1'});
+});
+
+$('.social').mouseout(function(){
+  $(this).css({'color':'white'})
+});
+
+});
